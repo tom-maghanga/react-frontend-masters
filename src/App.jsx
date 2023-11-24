b20b1e902df2a6606a9603ba58482d41e61e5314
Thomas Maghanga <142288589+tom-maghanga@users.noreply.github.com>
2023-11-24 07:34:25 +0300

    Added all context

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,38 @@
-import { useState } from "react";
-import React from "react";
 import { createRoot } from "react-dom/client";
-import SearchParams from "./SearchParams";
-import { Link, BrowserRouter,Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import Details from "./Details";
+import { useState } from "react";
 import AdoptedPetContext from "./AdoptedPetContext";
+import Details from "./Details";
+import SearchParams from "./SearchParams";
 
-
-
-const queryClient  = new QueryClient({
-  defaultOptions:{
-    queries:{
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
       staleTime: Infinity,
-      cacheTime : Infinity,
-    }
-  }
-})
+      cacheTime: Infinity,
+    },
+  },
+});
 
 const App = () => {
+  const adoptedPet = useState(null);
   return (
-   <BrowserRouter>
-    <QueryClientProvider client={queryClient}>
-      <header>
-        <Link to="/">Adopt me !</Link></header>
-
-          <Routes>
+    <div>
+      <BrowserRouter>
+        <AdoptedPetContext.Provider value={adoptedPet}>
+          <QueryClientProvider client={queryClient}>
+            <header>
+              <Link to="/">Adopt Me!</Link>
+            </header>
+            <Routes>
               <Route path="/details/:id" element={<Details />} />
               <Route path="/" element={<SearchParams />} />
             </Routes>
-            
-      </QueryClientProvider >
-   </BrowserRouter>
+          </QueryClientProvider>
+        </AdoptedPetContext.Provider>
+      </BrowserRouter>
+    </div>
   );
 };
 
